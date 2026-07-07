b06926f1daea6e0ad9f015bac8ecb67d5fd81e96
miljaar <tom_wyli@telenet.be>
2026-07-07 14:46:00 +0200

    add funny book seeder

--- BookTracker.Api.Tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ BookTracker.Api.Tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookTracker.Api.Tests.IntegrationTests;
@@ -15,6 +16,15 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(
+                new Dictionary<string, string?>
+                {
+                    ["SeedDatabase"] = "false"
+                });
+        });
+
         builder.ConfigureServices(services =>
         {
             var descriptor = services.SingleOrDefault(service =>
