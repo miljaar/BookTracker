80c0ce530de19e0e21d1804045b614cc0b912f2a
miljaar <tom_wyli@telenet.be>
2026-07-20 09:52:49 +0200

    add frontend book list

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import { AccountPage } from "./auth/AccountPage";
 import { LoginPage } from "./auth/LoginPage";
 import { LogoutButton } from "./auth/LogoutButton";
+import { BookListPage } from "./books/bookListPage";
 
 function HomePage() {
   return <h1>Book Tracker</h1>;
@@ -14,6 +15,7 @@ export default function App() {
         <Link to="/">Home</Link>{" "}
         <Link to="/login">Log in</Link>{" "}
         <Link to="/account">Account</Link>{" "}
+        <Link to="/books">Books</Link>{" "}
         <LogoutButton />
       </nav>
 
@@ -21,6 +23,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/books" element={<BookListPage />} />
       </Routes>
     </>
   )
